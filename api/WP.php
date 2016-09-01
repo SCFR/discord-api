c138fd3e62a0d1b16a1ed3915765861ff1d3edbe
Super d <superd001@gmail.com>
2016-09-01 08:47:38 +0200

    Token system for non loggin

--- api/WP.php
+++ api/WP.php
@@ -5,15 +5,15 @@ class WP {
   function __construct() {
     global $wpdb;
     $this->db = $wpdb;
-    add_action('init', array(&$this, "setCurrentUser"));
+
     add_action('rest_api_init',array(&$this, 'register_api_endpoints') );
+    add_action('rest_api_init',array(&$this, 'setCurrentUser') );
   }
 
   public function setCurrentUser() {
-      global $current_user;
-      $this->user = $current_user;
-      $this->loggedIn = $this->user->data->ID > 0 ? true : false;
-      $this->userId = $this->user->data->ID;
+    $this->user = new \SCFRDiscord\api\SCFRUser();
+
+    $this->loggedIn = $this->user->check_login();
   }
 
   public function register_api_endpoints() {
@@ -33,16 +33,52 @@ class WP {
       'methods' => 'POST',
       'callback' => array( &$this, 'test' ),
     ) );
+
+    \register_rest_route( $namespace, '/Login/', array(
+      'methods'  => 'GET',
+      'callback' => array( &$this, 'do_login' ),
+    ) );
   }
+
+  public function register_api_forum_endpoints() {
+    $namespace = 'Discord';
+
+
+  }
+
 
   private function error_connected() {
     if($this->loggedIn) return false;
     else return "USER_NOT_LOGGED_IN";
   }
 
+  public function validate_html() {
+
+  }
+
   public function is_connected() {
     header("Access-Control-Allow-Origin: *");
       return \SCFRDiscord\helper\APIReturn::message($this->loggedIn);
+  }
+
+  public function do_login($args) {
+
+
+
+    $phpbb = new \WPHPBB\Controller\Phpbb();
+    $phpbb->make_phpbb_env();
+
+    if()
+
+    if($phpbb->user->data['is_registered']) {
+       return \SCFRDiscord\helper\APIReturn::message("USER_LOGGED_IN");
+    }
+    else {
+       //if ($result['status'] == LOGIN_SUCCESS) return \SCFRDiscord\helper\APIReturn::message("USER_LOGGED_IN");
+       //else return \SCFRDiscord\helper\APIReturn::message("USER_LOGGED_IN", $result['error_msg']);
+
+       return get_relevant_header();
+    }
   }
 
   public function get_user_info() {
